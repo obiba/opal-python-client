232d201074088addd63e2a54a977d5fefb064963
Yannick Marcon <yannick.marcon@obiba.org>
2018-11-02 19:11:32 +0100

    #3313 Export data using plugin

---
+++ src/main/python/opal/export_plugin.py
@@ -0,0 +1,54 @@
+"""
+Data export to a datasource plugin.
+"""
+
+import sys
+import opal.core
+import opal.io
+import json
+
+
+def add_arguments(parser):
+    """
+    Add data command specific options
+    """
+    parser.add_argument('--datasource', '-d', required=True, help='Datasource name')
+    parser.add_argument('--tables', '-t', nargs='+', required=True, help='The list of tables to be exported')
+    parser.add_argument('--name', '-n', required=True, help='Opal datasource plugin name')
+    parser.add_argument('--config', '-c', required=True, help='A JSON file containing the export configuration')
+    parser.add_argument('--incremental', '-i', action='store_true', help='Incremental export')
+    parser.add_argument('--identifiers', '-id', required=False, help='Name of the ID mapping')
+    parser.add_argument('--json', '-j', action='store_true', help='Pretty JSON formatting of the response')
+
+
+def do_command(args):
+    """
+    Execute export data command
+    """
+    # Build and send request
+    try:
+
+        configStr = json.dumps(json.loads(open(args.config).read()))
+        client = opal.core.OpalClient.build(opal.core.OpalClient.LoginInfo.parse(args))
+        exporter = opal.io.OpalExporter.build(client=client, datasource=args.datasource, tables=args.tables,
+                                              identifiers=args.identifiers, output=configStr,
+                                              incremental=args.incremental, verbose=args.verbose)
+
+        # print result
+        response = exporter.submit(args.name)
+
+        # format response
+        res = response.content
+        if args.json:
+            res = response.pretty_json()
+
+        # output to stdout
+        print res
+
+    except Exception, e:
+        print e
+        sys.exit(2)
+    except pycurl.error, error:
+        errno, errstr = error
+        print >> sys.stderr, 'An error occurred: ', errstr
+        sys.exit(2)
