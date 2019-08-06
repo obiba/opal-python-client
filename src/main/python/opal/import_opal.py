be6b73da1c504e558afab482e40ffa7929e678e3
Yannick Marcon <yannick.marcon@obiba.org>
2019-08-06 17:17:07 +0200

    obiba/opal#3508 Opal import python command supports token authentication

--- src/main/python/opal/import_opal.py
+++ src/main/python/opal/import_opal.py
@@ -12,8 +12,9 @@ def add_arguments(parser):
     Add data command specific options
     """
     parser.add_argument('--ropal', '-ro', required=True, help='Remote Opal server base url')
-    parser.add_argument('--ruser', '-ru', required=True, help='Remote User name')
-    parser.add_argument('--rpassword', '-rp', required=True, help='Remote User password')
+    parser.add_argument('--ruser', '-ru', required=False, help='Remote user name (exclusive from using token)')
+    parser.add_argument('--rpassword', '-rp', required=False, help='Remote user password (exclusive from using token)')
+    parser.add_argument('--rtoken', '-rt', required=False, help='Remote personal access token (exclusive from user credentials)')
     parser.add_argument('--rdatasource', '-rd', required=True, help='Remote datasource name')
     # non specific import arguments
     opal.io.add_import_arguments(parser)
@@ -28,19 +29,21 @@ def do_command(args):
         importer = opal.io.OpalImporter.build(client=client, destination=args.destination, tables=args.tables,
                                               incremental=args.incremental, limit=args.limit, identifiers=args.identifiers,
                                               policy=args.policy, verbose=args.verbose)
-        # print result
-        extension_factory = OpalExtensionFactory(ropal=args.ropal, rdatasource=args.rdatasource, ruser=args.ruser,
-                                                 rpassword=args.rpassword)
-        response = importer.submit(extension_factory)
+        # remote opal client factory
+        if (args.rtoken and args.ruser) or (not args.rtoken and not args.ruser):
+            print("Either specify token OR user credentials (user name and password)")
+        else:
+            extension_factory = OpalExtensionFactory(ropal=args.ropal, rdatasource=args.rdatasource, ruser=args.ruser,
+                                                     rpassword=args.rpassword, rtoken=args.rtoken)
+            response = importer.submit(extension_factory)
 
-        # format response
-        res = response.content
-        if args.json:
-            res = response.pretty_json()
+            # format response
+            res = response.content
+            if args.json:
+                res = response.pretty_json()
 
-        # output to stdout
-        print res
-
+            # output to stdout
+            print res
 
     except Exception, e:
         print e
@@ -52,11 +55,12 @@ def do_command(args):
 
 
 class OpalExtensionFactory(opal.io.OpalImporter.ExtensionFactoryInterface):
-    def __init__(self, ropal, rdatasource, ruser, rpassword):
+    def __init__(self, ropal, rdatasource, ruser, rpassword, rtoken):
         self.ropal = ropal
         self.rdatasource = rdatasource
         self.ruser = ruser
         self.rpassword = rpassword
+        self.rtoken = rtoken
 
     def add(self, factory):
         """
@@ -64,8 +68,11 @@ class OpalExtensionFactory(opal.io.OpalImporter.ExtensionFactoryInterface):
         """
         rest_factory = factory.Extensions[opal.protobuf.Magma_pb2.RestDatasourceFactoryDto.params]
         rest_factory.url = self.ropal
-        rest_factory.username = self.ruser
-        rest_factory.password = self.rpassword
+        if self.rtoken:
+            rest_factory.token = self.rtoken
+        else:
+            rest_factory.username = self.ruser
+            rest_factory.password = self.rpassword
         rest_factory.remoteDatasource = self.rdatasource
 
 
