f82028b799ed379cd55eb7e920736d7f49759ddf
Yannick Marcon <yannick.marcon@obiba.org>
2020-06-04 17:30:03 +0200

    obiba/opal#3563 restore-project added

--- src/main/python/opal/restore_project.py
+++ src/main/python/opal/restore_project.py
@@ -1,5 +1,5 @@
 """
-Backup a project: launch a backup task.
+Restore a project: launch a restore task.
 """
 
 import re
@@ -11,26 +11,29 @@ def add_arguments(parser):
     """
     Add command specific options
     """
-    parser.add_argument('--project', '-pr', required=True, help='Source project name')
-    parser.add_argument('--archive', '-ar', required=True, help='Archive directory path in the Opal file system')
-    parser.add_argument('--force', '-f', action='store_true', help='Force overwriting an existing backup folder.')
+    parser.add_argument('--project', '-pr', required=True, help='Destination project name (must exist)')
+    parser.add_argument('--archive', '-ar', required=True, help='Archive directory or zip file path in the Opal file system')
+    parser.add_argument('--arpassword', '-arp', required=False, help='Password to decrypt zip archive (optional)')
+    parser.add_argument('--force', '-f', action='store_true', help='Force overwriting existing items (table, view, resource, report). Files override is not checked.')
     parser.add_argument('--json', '-j', action='store_true', help='Pretty JSON formatting of the response')
 
 def do_command(args):
     """
-    Prepare the backup parameters and launch the backup task on the project
+    Prepare the restore parameters and launch the restore task on the project
     """
 
     # Build and send request
     try:
-        # backup options
-        options = opal.protobuf.Commands_pb2.BackupCommandOptionsDto()
+        # restore options
+        options = opal.protobuf.Commands_pb2.RestoreCommandOptionsDto()
         options.archive = args.archive
         if args.force:
             options.override = args.force
+        if args.arpassword:
+            options.password = args.arpassword
 
         client = opal.core.OpalClient.build(opal.core.OpalClient.LoginInfo.parse(args))
-        uri = opal.core.UriBuilder(['project', args.project, 'commands', '_backup']).build()
+        uri = opal.core.UriBuilder(['project', args.project, 'commands', '_restore']).build()
         request = client.new_request()
         request.fail_on_error().accept_json().content_type_protobuf()
         if args.verbose:
