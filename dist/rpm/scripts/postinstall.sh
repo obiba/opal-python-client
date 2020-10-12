247bc8a24fb8af0913fd69c884453282703db1ed
Yannick Marcon <yannick.marcon@obiba.org>
2020-10-12 09:50:11 +0200

    #17 python3 dist-packages is system dependent

--- dist/rpm/scripts/postinstall.sh
+++ dist/rpm/scripts/postinstall.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-# link opal folder to default python lib, /usr/lib/python3/dist-packages may not be icluded in the lib path
-python2_lib=/usr/lib/python2.7/dist-packages
+# link opal folder to default python lib, /usr/share/pyshared may not be icluded in the lib path
+python_lib=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 
 case "$1" in
   0)
-    if [ -d $python2_lib ]; then
-      rm -f $python2_lib/opal
+    if [ -d $python_lib ]; then
+      rm -rf $python_lib/opal
     fi
   ;;
 
   [1-2])
-    if [ -d $python2_lib ]; then
-      rm -f $python2_lib/opal
+    if [ -d $python_lib ]; then
+      rm -rf $python_lib/opal
     fi
+    ln -s /usr/share/pyshared/opal $python_lib/opal
   ;;
 
   *)
