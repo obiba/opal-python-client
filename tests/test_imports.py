bb06d6a89b91fb5cc1bacbf4d8d6951ebd8ce427
Yannick Marcon <yannick.marcon@obiba.org>
2023-04-28 15:02:13 +0200

    Fix concurrent import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,8 @@
 from obiba_opal import ImportCSVCommand, TaskService, FileService, DictionaryService
 from tests.utils import make_client
 import random
+import shutil
+import os
 
 class TestClass:
 
@@ -16,17 +18,22 @@ class TestClass:
     def test_csv(self):
         client = self.client
         fs = FileService(client)
-        fs.upload_file('./tests/resources/data.csv', '/tmp')
-        assert fs.file_info('/tmp/data.csv') is not None
+        id = random.choice(list(range(1, 999, 1)))
+        inname = 'data%s' % id
+        inpath = '/tmp/%s.csv' % inname
+        shutil.copy('./tests/resources/data.csv', inpath)
+        fs.upload_file(inpath, '/tmp')
+        os.remove(inpath)
+        assert fs.file_info(inpath) is not None
         service = ImportCSVCommand(client)
-        task = service.import_data('/tmp/data.csv', 'CNSIM')
+        task = service.import_data(inpath, 'CNSIM')
         assert 'id' in task
         status = TaskService(client).wait_task(task['id'])
         assert status in ['SUCCEEDED', 'CANCELED', 'FAILED']
-        fs.delete_file('/tmp/data.csv')
+        fs.delete_file(inpath)
         dico = DictionaryService(client)
-        table = dico.get_table('CNSIM', 'data')
+        table = dico.get_table('CNSIM', inname)
         assert table is not None
-        dico.delete_tables('CNSIM', ['data'])
+        dico.delete_tables('CNSIM', [inname])
         ds = dico.get_datasource('CNSIM')
-        assert 'data' not in ds['table']
+        assert inname not in ds['table']
