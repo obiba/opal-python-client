541e5f1f6b799254dac42cf32ddf2536bbb7340a
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2023-06-01 15:22:48 -0400

    Added TEST server, user, password in utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,11 @@
 from obiba_opal import OpalClient
 
+# TEST_SERVER = 'https://mica-demo.obiba.org'
+TEST_SERVER = 'http://localhost:8080'
+TEST_USER = 'administrator'
+TEST_PASSWORD = 'password'
+
+
 def make_client():
-  return OpalClient.buildWithAuthentication(server='https://opal-demo.obiba.org', user='administrator', password='password')
-  
+  # return OpalClient.buildWithAuthentication(server='https://opal-demo.obiba.org', user='administrator', password='password')
+  return OpalClient.buildWithAuthentication(server='http://localhost:8080', user='administrator', password='password')
