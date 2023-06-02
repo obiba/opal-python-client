7d7671077a3ab2427ed06899077eda47e3774051
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2023-06-02 16:55:11 -0400

    Fixed test server

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,6 @@
 from obiba_opal import OpalClient
 
-TEST_SERVER = 'https://mica-demo.obiba.org'
+TEST_SERVER = 'https://opal-demo.obiba.org'
 # TEST_SERVER = 'http://localhost:8080'
 TEST_USER = 'administrator'
 TEST_PASSWORD = 'password'
